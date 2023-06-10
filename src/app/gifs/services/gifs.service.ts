1156e9ed74505885f56166e6a0e7bb69f1e83741
oluciams <oluciams84@gmail.com>
2023-06-10 13:43:41 -0500

    make Http request

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,12 +1,18 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchResponse, Gif } from '../interfaces/gifs.interfaces';
 
-@Injectable({providedIn: 'root'})
+
+@Injectable({ providedIn: 'root' })
 export class GifsService {
 
+  public gifList: Gif[] = [];
+
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'CH0hOvJYwjC19pdz2hCw0JvXuUm3YGil';
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
 
-  constructor() { }
-
+  constructor(private http: HttpClient) {}
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -16,16 +22,27 @@ export class GifsService {
     tag = tag.toLocaleLowerCase();
 
     if (this._tagsHistory.includes(tag)) {
-      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
     }
 
     this._tagsHistory.unshift(tag);
     this._tagsHistory = this.tagsHistory.splice(0, 10);
   }
 
-  public searchTag(tag: string): void {
+  async searchTag(tag: string): Promise<void> {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
 
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+
+        this.gifList = resp.data;
+        console.log({ gifs: this.gifList });
+      });
   }
 }
